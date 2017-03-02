676c8c5acb2f115adc13180581fb371aa6eb99ba
adridevparis <mama0304>
2017-03-02 23:45:22 +0100

    Light changes for defibrillator + add descent

--- Codingame/Defibrillator.fs
+++ Codingame/Defibrillator.fs
@@ -18,14 +18,13 @@ let getPointRad pDeg = {LongRad=getRad pDeg.LongDeg; LatRad=getRad pDeg.LatDeg}
 let getPointDeg (lon:string) (lat:string) = {LongDeg=float(lon.Replace(",", ".")); LatDeg=float(lat.Replace(",", "."))}
 
 let rec getDefibs nbLeft =
-    match nbLeft with
-    | 0 -> []
-    | a ->
+    if nbLeft = 0 then []
+    else
         let defib = Console.In.ReadLine()
         let defibSplit = defib.Split [|';'|]
         let pDeg = getPointDeg defibSplit.[4] defibSplit.[5]
         let pRad = {Number=int(defibSplit.[0]); Name=defibSplit.[1]; Adress=defibSplit.[2]; Tel=defibSplit.[3]; PointR=getPointRad pDeg}
-        pRad::(getDefibs (a-1))
+        pRad::(getDefibs (nbLeft-1))
 
 let defibs = getDefibs N
 
---
+++ Codingame/Descent.fs
@@ -0,0 +1,20 @@
+﻿module Descent
+
+(* The while loop represents the game. *)
+(* Each iteration represents a turn of the game *)
+(* where you are given inputs (the heights of the mountains) *)
+(* and where you have to print an output (the index of the mountain to fire on) *)
+(* The inputs you are given are automatically updated according to your last actions. *)
+open System
+
+(* game loop *)
+while true do
+    let mutable maxHeight = -1
+    let mutable indexMaxHeight = -1
+    for i in 0 .. 8 - 1 do
+        let mountainH = int(Console.In.ReadLine()) (* represents the height of one mountain. *)
+        if mountainH > maxHeight then
+            indexMaxHeight <- i
+            maxHeight <- mountainH
+
+    printfn "%d" indexMaxHeight
