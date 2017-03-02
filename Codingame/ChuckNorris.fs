f5d6113030903edfa92a8282f6f02dd1cacd9eef
Adri <adrien.marion@ensta.org>
2017-03-02 18:08:04 +0100

    Chuck norris + Power of thor

---
+++ Codingame/ChuckNorris.fs
@@ -0,0 +1,36 @@
+﻿module ChuckNorris
+
+(* Auto-generated code below aims at helping you parse *)
+(* the standard input according to the problem statement. *)
+open System
+
+let MESSAGE = Console.In.ReadLine()
+
+(* Write an action using printfn *)
+(* To debug: Console.Error.WriteLine("Debug message") *)
+
+let rec convertToBase b nb n =
+    let u = n % b
+    let rest = (n-u)/b
+    match nb = 1 with
+    | true -> string(u)
+    | false -> (convertToBase b (nb-1) rest) + string(u)
+
+let rec convertToString intList =
+    match intList with
+    | [] -> ""
+    | a::b -> (convertToBase 2 7 a) + convertToString b
+
+let messageAscii = MESSAGE |> Seq.map int |> Seq.toList |> convertToString |> Seq.map string |> Seq.toList
+
+let rec convertToChuck previousChar strList =
+    match strList with
+    | [] -> ""
+    | a::b when a = previousChar -> "0" + convertToChuck previousChar b
+    | a::b when a = "1" -> (if previousChar = "" then "" else " ") + "0 0" + convertToChuck a b
+    | a::b when a = "0" -> (if previousChar = "" then "" else " ") + "00 0" + convertToChuck a b
+    | _ -> ""
+
+let messageChuck = convertToChuck "" messageAscii
+
+printfn "%s" messageChuck
---
+++ Codingame/PowerOfThor1.fs
@@ -0,0 +1,40 @@
+﻿module PowerOfThor1
+
+(* Auto-generated code below aims at helping you parse *)
+(* the standard input according to the problem statement. *)
+(* --- *)
+(* Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders. *)
+open System
+
+(* lightX: the X position of the light of power *)
+(* lightY: the Y position of the light of power *)
+(* initialTX: Thor's starting X position *)
+(* initialTY: Thor's starting Y position *)
+let token = (Console.In.ReadLine()).Split [|' '|]
+let lightX = int(token.[0])
+let lightY = int(token.[1])
+let initialTX = int(token.[2])
+let initialTY = int(token.[3])
+
+let mutable currentX = initialTX
+let mutable currentY = initialTY
+
+let getSouthNorth curY goalY =
+    match curY - goalY with
+    | pos when pos > 0 && curY > 0 -> ("N", curY - 1)
+    | neg when neg < 0 && curY < 17 -> ("S", curY + 1)
+    | _ -> ("", curY)
+
+let getEastWest curX goalX =
+    match curX - goalX with
+    | pos when pos > 0 && curX > 0 -> ("W", curX - 1)
+    | neg when neg < 0 && curX < 39 -> ("E", curX + 1)
+    | _ -> ("", curX)
+
+while true do
+    // let remainingTurns = int(Console.In.ReadLine()) (* The remaining amount of turns Thor can move. Do not remove this line. *)
+    let (southNorth, newY) = getSouthNorth currentY lightY
+    let (eastWest, newX) = getEastWest currentX lightX
+    currentX <- newX
+    currentY <- newY
+    printfn "%s%s" southNorth eastWest
